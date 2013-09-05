7463574c38f4e1c24fb56571fc8bc2d45121df65
Alex Kozlov <ati1987@gmail.com>
2013-09-05 19:16:49 +0300

    footer added; carousel styled

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -9,15 +9,54 @@
 </head>
 <body>
     <div class="content">
+            
         <header>            
             <h1 class="sprites header-logo"><a href="<?php echo base_url();?>" class="header-logo-link">Your Name's Not Down</a></h1>  
 
             <ul class="header-nav">
-                <li><a href="/about" class="header-nav-link">About</a></li>
-                <li><a href="/register" class="header-nav-link">Register</a></li>
-                <li><a href="/signin" class="header-nav-link">Sign in</a></li>
+                <li><a href="" class="header-nav-link">About</a></li>
+                <li><a href="" class="header-nav-link">Register</a></li>
+                <li><a href="" class="header-nav-link">Sign in</a></li>
             </ul>
         </header>
+        
+        <section class="logo-carousel-panel">
+            <span class="logo-carousel-side left"></span>
+            <span class="logo-carousel-side right"></span>
+            
+            <ul class="logo-carousel">
+                <li>
+                    <a href="" title="Club Name" style="background: url(<?php echo base_url();?>/static/img/club.png) 0 0; width: 206px; height: 63px;">Club Name</a>
+                </li>
+                <li>
+                    <a href="" title="Club Name" style="background: url(<?php echo base_url();?>/static/img/club-2.png) 0 0; width: 185px; height: 73px;">Club Name</a>
+                </li>
+                <li>
+                    <a href="" title="Club Name" style="background: url(<?php echo base_url();?>/static/img/club.png) 0 0; width: 206px; height: 63px;">Club Name</a>
+                </li>
+                <li>
+                    <a href="" title="Club Name" style="background: url(<?php echo base_url();?>/static/img/club-2.png) 0 0; width: 185px; height: 73px;">Club Name</a>
+                </li>
+                <li>
+                    <a href="" title="Club Name" style="background: url(<?php echo base_url();?>/static/img/club.png) 0 0; width: 206px; height: 63px;">Club Name</a>
+                </li>
+            </ul>
+        </section> 
+        
+        <footer>
+            <ul class="footer-nav">
+                <li><a href="" class="footer-nav-link no-border">Events</a></li>
+                <li><a href="" class="footer-nav-link">Club &amp; Venues</a></li>
+                <li><a href="" class="footer-nav-link">About</a></li>
+                <li><a href="" class="footer-nav-link">Terms &amp; Conditions</a></li>
+                <li><a href="" class="footer-nav-link">Privacy Policy</a></li>
+                <li><a href="" class="footer-nav-link no-border">Unsubscribe</a></li>
+                <li><a href="" class="footer-nav-link">Register</a></li>
+                <li><a href="" class="footer-nav-link">Sign in</a></li>
+            </ul>
+            
+            <span class="sprites footer-logo">Your Name's Not Down</span>
+        </footer>
     </div>
 </body>
 </html>
